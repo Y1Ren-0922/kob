a9176da756d5c6eea85ceb15d41bee51f6a559eb
Y1Ren <1360100881@qq.com>
2022-08-13 17:27:05 +0800

    注册登录管理模块(中)

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,6 +11,36 @@ import "bootstrap/dist/js/bootstrap"
 export default {
   components: {
     NavBar,
+  },
+
+  setup() {
+    // axios({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   method: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1ODY4YmMzZjRkZmI0ZjU5YTY1ZTFhYjEyODQ0MzJkYSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY2MDM3NTQ5OSwiZXhwIjoxNjYxNTg1MDk5fQ.BN8CQ6vZV5dqrl60JldnBqym1tXP86fISvn_a2cGq1M"
+    //   }
+    // }).then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.log(error.response.data);
+    // });
+
+    // axios({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   method: "post",
+    //   params: {
+    //     username: "e",
+    //     password: "",
+    //     confirmedPassword: "pd"
+    //   }
+    // }).then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.log(error.response.data);
+    // });
+
+
   }
 }
 
---
+++ web/src/store/user.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+
+export default {
+    state: {
+        id: "",
+        username: "",
+        photo: "",
+        token: "",
+        is_login: false,
+    },
+    getters: {
+    },
+    mutations: {
+        updateUser(state, user) {
+            state.id = user.id;
+            state.username = user.username;
+            state.photo = user.photo;
+            state.is_login = user.is_login;
+        },
+        updateToken(state, token) {
+            state.token = token;
+        },
+        logout(state) {
+            state.id = "",
+                state.username = "",
+                state.photo = "",
+                state.token = "",
+                state.is_login = false
+        },
+    },
+    actions: {
+        login(context, data) {
+            axios({
+                url: "http://127.0.0.1:3000/user/account/token/",
+                method: "post",
+                params: {
+                    username: data.username,
+                    password: data.password
+                }
+            }).then(function (response) {
+                if (response.data['error_message'] === "success") {
+                    context.commit("updateToken", response.data['token']);
+                    data.success(response);
+                } else {
+                    data.error(response);
+                }
+
+            }).catch(function (error) {
+                data.error(error.response)
+            });
+        },
+        getinfo(context, data) {
+            axios({
+                url: "http://127.0.0.1:3000/user/account/info/",
+                method: "get",
+                headers: {
+                    Authorization: "Bearer " + context.state.token,
+                }
+            }).then(function (response) {
+                if (response.data['error_message'] === 'success') {
+
+                    context.commit("updateUser", {
+                        ...response.data,
+                        is_login: true,
+                    })
+                    data.success(response.data)
+                } else {
+                    data.error(response.data);
+                }
+
+            }).catch(function (error) {
+                data.error(error.response)
+            });
+        },
+        logout(context) {
+            context.commit("logout");
+        }
+    },
+    modules: {
+    }
+}
